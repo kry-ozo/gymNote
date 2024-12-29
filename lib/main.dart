cfa1b3fecb726359767af8ecacefb9a359485232
Kryozo <dorentier124@gmail.com>
2024-12-29 20:38:48 +0100

    added first styles and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:gym_note/pages/workouts.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const GymNote());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class GymNote extends StatelessWidget {
+  const GymNote({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 43, 43, 43),
+          )
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        backgroundColor:const Color.fromARGB(255, 43, 43, 43) ,
+        appBar: AppBar(
+          title: const Text("GymNote", style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.w600),),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 24, 24, 24),
+          leading: Builder(
+            builder: (context) {
+              return IconButton(onPressed: (){
+                Scaffold.of(context).openDrawer();
+              }, icon: const Icon(Icons.menu, color: Colors.yellow,));
+            }
+          ),
         ),
+        drawer: Drawer(
+          backgroundColor: const Color.fromARGB(255, 43, 43, 43) ,
+          child: ListView(
+            children: [
+              DrawerHeader(child: Text("Settings", style: TextStyle(color: Colors.yellow),)),
+              ListTile(title: Text("Item"),),
+              ListTile(title: Text("Item"),),
+              ListTile(title: Text("Item"),)
+
+            ],
+          ),
+        ),
+        body: Workouts()
       ),
     );
   }
---
+++ lib/pages/workouts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Workouts extends StatelessWidget {
+  const Workouts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+
+      ],
+    );
+  }
+}
