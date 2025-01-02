44d6e862c7fb13a7ec13ab08a0a365a064c19e9a
Kryozo <dorentier124@gmail.com>
2025-01-02 15:26:55 +0100

    Added exercise model and updated worout model

---
+++ lib/models/exercise.dart
@@ -0,0 +1,9 @@
+class Exercise {
+  final String id;
+  final String name;
+  final List<DateTime> logs;
+  final String description;
+
+  Exercise({required this.id, required this.name, required this.logs, required this.description});
+
+}
--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -1,7 +1,9 @@
+import 'package:gym_note/models/exercise.dart';
+
 class Workout {
   final String id;
   final int exercisesCount;
-  final List<String> exercises;
+  final List<Exercise> exercises;
 
   Workout({required this.id, required this.exercisesCount, required this.exercises});
 }
